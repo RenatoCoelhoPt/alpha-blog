a9c43c301f56129661fa81ebdaee2be2d003ba6c
Renato Coelho <recoelho@deloitte.pt>
2022-05-22 21:28:22 +0100

    Created Articles form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,12 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    #@article = Article.new(title: params[:title], description: params[:description])
+  end
+
+  def create
+    render plain: params[:article]
+  end
 end
